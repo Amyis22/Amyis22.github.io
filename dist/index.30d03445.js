4e2a41c06ddb0aa27d9ac4df33e21eaeb1deda85
suxiawang <suxiawang@tencent.com>
2023-12-26 16:26:01 +0800

    feat: parcel docs

--- dist/index.30d03445.js
+++
@@ -1,2 +0,0 @@
-console.log("111hha, i am sophia wang.");
-//# sourceMappingURL=index.30d03445.js.map
---
+++ docs/index.4e559332.js
@@ -0,0 +1,2 @@
+console.log("111hha, i am sophia wang.");
+//# sourceMappingURL=index.4e559332.js.map
